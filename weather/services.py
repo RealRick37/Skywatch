c186cc8fdb0bf25038b7a8beb6bcccc145d7bce7
RealRick37 <122051997+RealRick37@users.noreply.github.com>
2026-09-12 14:53:41 +0330

    Normalize weather API response

--- weather/services.py
+++ weather/services.py
@@ -45,5 +45,60 @@ class WeatherService:
         response=httpx.get(cls.BASE_URL, params=params, timeout=10.0)
 
         response.raise_for_status()
+        data=response.json()
+        return cls._normalize_response(data)
 
-        return response.json()
+    @staticmethod
+    def _normalize_response(data):
+        hourly_data=data["hourly"]
+        daily_data=data["daily"]
+
+        hourly=[
+            {
+                "time": hourly_data["time"][i],
+                "temperature": hourly_data["temperature_2m"][i],
+                "feels_like": hourly_data["apparent_temperature"][i],
+                "precipitation_probability": hourly_data["precipitation_probability"][i],
+                "precipitation": hourly_data["precipitation"][i],
+                "wind_speed": hourly_data["wind_speed_10m"][i],
+                "weather_code": hourly_data["weather_code"][i],
+            }
+            for i in range(len(hourly_data["time"]))
+        ]
+
+        daily=[
+            {
+                "date": daily_data["time"][i],
+                "temperature_max": daily_data["temperature_2m_max"][i],
+                "temperature_min": daily_data["temperature_2m_min"][i],
+                "precipitation_probability": daily_data["precipitation_probability_max"][i],
+                "precipitation": daily_data["precipitation_sum"][i],
+                "wind_speed_max": daily_data["wind_speed_10m_max"][i],
+                "weather_code": daily_data["weather_code"][i],
+                "sunrise": daily_data["sunrise"][i],
+                "sunset": daily_data["sunset"][i],
+            }
+            for i in range(len(daily_data["time"]))
+        ]
+
+        return {
+            "location": {
+                "latitude": data["latitude"],
+                "longitude": data["longitude"],
+                "timezone": data["timezone"],
+            },
+            "current": {
+                "time": data["current"]["time"],
+                "temperature": data["current"]["temperature_2m"],
+                "feels_like": data["current"]["apparent_temperature"],
+                "humidity": data["current"]["relative_humidity_2m"],
+                "wind_speed": data["current"]["wind_speed_10m"],
+                "pressure": data["current"]["pressure_msl"],
+                "visibility": data["current"]["visibility"],
+                "uv_index": data["current"]["uv_index"],
+                "precipitation": data["current"]["precipitation"],
+                "weather_code": data["current"]["weather_code"],
+            },
+            "hourly": hourly,
+            "daily": daily,
+        }
