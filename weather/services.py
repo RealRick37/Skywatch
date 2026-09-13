b8988614ab6098f77ed0778feeff400400066806
RealRick37 <122051997+RealRick37@users.noreply.github.com>
2026-09-13 14:04:26 +0330

    Add weather condition

--- weather/services.py
+++ weather/services.py
@@ -48,8 +48,8 @@ class WeatherService:
         data=response.json()
         return cls._normalize_response(data)
 
-    @staticmethod
-    def _normalize_response(data):
+    @classmethod
+    def _normalize_response(cls, data):
         hourly_data=data["hourly"]
         daily_data=data["daily"]
 
@@ -62,6 +62,7 @@ class WeatherService:
                 "precipitation": hourly_data["precipitation"][i],
                 "wind_speed": hourly_data["wind_speed_10m"][i],
                 "weather_code": hourly_data["weather_code"][i],
+                "condition": cls.get_weather_condition(hourly_data["weather_code"][i]),
             }
             for i in range(len(hourly_data["time"]))
         ]
@@ -75,6 +76,7 @@ class WeatherService:
                 "precipitation": daily_data["precipitation_sum"][i],
                 "wind_speed_max": daily_data["wind_speed_10m_max"][i],
                 "weather_code": daily_data["weather_code"][i],
+                "condition": cls.get_weather_condition(daily_data["weather_code"][i]),
                 "sunrise": daily_data["sunrise"][i],
                 "sunset": daily_data["sunset"][i],
             }
@@ -98,7 +100,44 @@ class WeatherService:
                 "uv_index": data["current"]["uv_index"],
                 "precipitation": data["current"]["precipitation"],
                 "weather_code": data["current"]["weather_code"],
+                "condition": cls.get_weather_condition(data["current"]["weather_code"]),
             },
             "hourly": hourly,
             "daily": daily,
         }
+
+
+    @staticmethod
+    def get_weather_condition(weather_code):
+        conditions={
+            0: "Clear sky",
+            1: "Mainly clear",
+            2: "Partly cloudy",
+            3: "Overcast",
+            45: "Fog",
+            48: "Depositing rime fog",
+            51: "Light drizzle",
+            53: "Moderate drizzle",
+            55: "Dense drizzle",
+            56: "Light freezing drizzle",
+            57: "Dense freezing drizzle",
+            61: "Slight rain",
+            63: "Moderate rain",
+            65: "Heavy rain",
+            66: "Light freezing rain",
+            67: "Heavy freezing rain",
+            71: "Slight snow",
+            73: "Moderate snow",
+            75: "Heavy snow",
+            77: "Snow grains",
+            80: "Slight rain showers",
+            81: "Moderate rain showers",
+            82: "Violent rain showers",
+            85: "Slight snow showers",
+            86: "Heavy snow showers",
+            95: "Thunderstorm",
+            96: "Thunderstorm with slight hail",
+            99: "Thunderstorm with heavy hail",
+        }
+
+        return conditions.get(weather_code, "Unknown")
